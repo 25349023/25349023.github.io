e4cacb670bf87eef93bacf99ebcf438c289fb3ab
Skyline <25349023.qq@gmail.com>
2021-02-06 20:54:20 +0800

    [UPDATE] table of contents, adjacent posts

--- assets/static/js/scroll_top.js
+++ assets/static/js/scroll_top.js
@@ -1,12 +1,12 @@
 let mybutton = document.getElementById("scroll-top");
 
-window.onscroll = function scrollFunction() {
-  if (
-    document.body.scrollTop > 240 ||
-    document.documentElement.scrollTop > 240
-  ) {
-    mybutton.classList.remove("hidden");
-  } else {
-    mybutton.classList.add("hidden");
-  }
-};
+window.addEventListener("scroll", function () {
+    if (
+        document.body.scrollTop > 500 ||
+        document.documentElement.scrollTop > 500
+    ) {
+        mybutton.classList.remove("hidden");
+    } else {
+        mybutton.classList.add("hidden");
+    }
+});
