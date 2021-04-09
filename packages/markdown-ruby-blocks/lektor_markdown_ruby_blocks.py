46f8ea78f234ea25f81200dbf15f34c25cf6f275
Skyline <25349023.qq@gmail.com>
2021-04-09 20:24:10 +0800

    [UPDATE] support for multi-word in one ruby pattern

--- packages/markdown-ruby-blocks/lektor_markdown_ruby_blocks.py
+++ packages/markdown-ruby-blocks/lektor_markdown_ruby_blocks.py
@@ -1,11 +1,19 @@
-import re
+from collections import namedtuple
 from html import escape
+import re
 from lektor.pluginsystem import Plugin
 
 # group 1: color of the text, group 2: the content
 rb_block_pattern = re.compile(r'^\^\^(#[0-9a-fA-F]{6,8})?\n(.*)\^\^$', flags=re.DOTALL)
 # group 1: base text, group 2: ruby text
 ruby_pattern = re.compile(r'\((.+?)\)\[(.+?)]')
+
+
+def convert(base, ruby):
+    return f'{base}<rp>(</rp><rt>{ruby}</rt><rp>)</rp>'
+
+
+Convertion = namedtuple('Convertion', 'span text')
 
 
 class RubyBlockMixin:
@@ -17,7 +25,7 @@ class RubyBlockMixin:
         if match is None:
             return super().paragraph(text)
         color = match.group(1) if match.group(1) is not None else '#5c443a'
-        ruby_text = self._rubify(match.group(2).strip(), color)
+        ruby_text = self._rubify(escape(match.group(2).strip()), color)
 
         return f'<p>{ruby_text}</p>'
 
@@ -35,13 +43,25 @@ class RubyBlockMixin:
     def _rubify_line(self, line, color):
         if line == "-##-":
             return ''
-        rb_text = ruby_pattern.sub(r'\1<rp>(</rp><rt>\2</rt><rp>)</rp>', line)
+
+        cvts = self._collect_convertions(line)
+        for (start, end), rt in cvts[::-1]:
+            line = line[:start] + rt + line[end:]
+
         if color is not None:
-            rb_line = f'<span class="ruby-line" style="color: {color};"><ruby>{rb_text}</ruby></span>'
+            rb_line = f'<span class="ruby-line" style="color: {color};"><ruby>{line}</ruby></span>'
         else:
-            rb_line = f'<span class="ruby-line"><ruby>{rb_text}</ruby></span>'
+            rb_line = f'<span class="ruby-line">{line}</span>'
 
         return rb_line
+
+    def _collect_convertions(self, line):
+        replacement = []
+        for match in ruby_pattern.finditer(line):
+            base, ruby = match.group(1).split('|'), match.group(2).split('|')
+            cvt_text = ''.join(convert(b, r) for b, r in zip(base, ruby))
+            replacement.append(Convertion(match.span(), cvt_text))
+        return replacement
 
 
 class RubyBlockPlugin(Plugin):
